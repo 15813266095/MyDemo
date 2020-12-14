fd611435ae19836932ba53be1231688c4d2c81e0
zkw <920989427@qq.com>
2020-12-14 18:30:38 +0800

    MyDemo, add, 完成客户端的分发机制和交互页面，服务端实现与数据库连接

---
+++ demo-client/src/main/java/com/zkw/springboot/controller/ClientController.java
@@ -0,0 +1,71 @@
+package com.zkw.springboot.controller;
+
+import com.zkw.springboot.bean.User;
+import com.zkw.springboot.protocol.Message;
+import com.zkw.springboot.protocol.MessageType;
+import com.zkw.springboot.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @RequestMapping(value = {"/","/index"})
+    public String hello(Model model){
+        model.addAttribute("user",new User());
+        service.start("localhost", 8881);
+        return "index";
+    }
+
+    @PostMapping(value = "/register")
+    public String register(User user,Model model){
+        Message message = service.register(user);
+        model.addAttribute("user",message.getUser());
+        model.addAttribute("msg",message.getDescription());
+        return "homepage";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@RequestParam(name="action")String action,User user,Model model){
+        if(action.equals("register")){
+            return "register";
+        }
+        Message message = service.login(user.getAccount(), user.getPassword());
+        model.addAttribute("user",message.getUser());
+        model.addAttribute("msg",message.getDescription());
+        if(message.getMessageType()== MessageType.ERROR){
+            return "index";
+        }
+        return "homepage";
+    }
+
+    @PostMapping(value = "/operator",params="move")
+    public String move(User user, @RequestParam(name="move") String direction, Model model){
+        Message message = service.move(direction,user);
+        model.addAttribute("user",message.getUser());
+        model.addAttribute("msg",message.getDescription());
+        return "homepage";
+    }
+
+    @PostMapping(value = "/operator",params="changeScenes")
+    public String changeScenes(User user,@RequestParam(name="changeScenes")String scenes,Model model){
+        user.setScenes(scenes);
+        Message message = service.changeScenes(user);
+        model.addAttribute("user",message.getUser());
+        model.addAttribute("msg",message.getDescription());
+        return "homepage";
+    }
+
+    @PostMapping(value = "/operator",params="disconnect")
+    public String disconnect(User user,Model model){
+        service.disconnect(user);
+        return "redirect:/index";
+    }
+}
